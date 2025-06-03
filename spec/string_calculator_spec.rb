3a4f1a6d6a0ba00a7ce4707a2161f04db7f4686e
Vishal Jain <v.jain@somaiya.edu>
2025-06-03 21:11:03 +0530

    Spec for empty string returning 0

---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,10 @@
+require 'string_calculator'
+
+RSpec.describe StringCalculator do
+  describe '#add' do
+    it 'returns 0 for an empty string' do
+      calculator = StringCalculator.new
+      expect(calculator.add('')).to eq(0)
+    end
+  end
+end
