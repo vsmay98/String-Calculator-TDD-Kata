1de1622c9d8fcaa08db830bc335826895b6fd72f
Vishal Jain <v.jain@somaiya.edu>
2025-06-04 19:41:00 +0530

    Spec to handle newlines as delimiters between numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -19,5 +19,9 @@ RSpec.describe StringCalculator do
     it 'handles multiple comma-separated numbers' do
       expect(calculator.add('1,2,3,4')).to eq(10)
     end
+
+    it 'handles newlines between numbers' do
+      expect(calculator.add("1\n2,3")).to eq(6)
+    end
   end
 end
