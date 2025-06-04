b14f03ee4136871e918bc60b229a87889fd439b8
Vishal Jain <v.jain@somaiya.edu>
2025-06-04 19:21:07 +0530

    Spec to return the number for a single number DRY up spec file

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,10 +1,15 @@
 require 'string_calculator'
 
 RSpec.describe StringCalculator do
+  subject(:calculator) { described_class.new }
+
   describe '#add' do
     it 'returns 0 for an empty string' do
-      calculator = StringCalculator.new
       expect(calculator.add('')).to eq(0)
+    end
+
+    it 'returns the number for a single number' do
+      expect(calculator.add('1')).to eq(1)
     end
   end
 end
