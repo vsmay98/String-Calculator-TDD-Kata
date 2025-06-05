032c4e3685e33a485077c6ca8772145e39d6d446
Vishal Jain <v.jain@somaiya.edu>
2025-06-05 19:37:54 +0530

    Update StringCalculator#add to throw exception for negative numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -9,6 +9,10 @@ class StringCalculator
       numbers = numbers.split("\n", 2).last
     end
 
-    numbers.split(delimiter).map(&:to_i).sum
+    numbers_array = numbers.split(delimiter).map(&:to_i)
+    negatives = numbers_array.filter { |n| n < 0 }
+    raise "negative numbers not allowed #{negatives.join(',')}" unless negatives.empty?
+
+    numbers_array.sum
   end
 end
